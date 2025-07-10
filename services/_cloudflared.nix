560c3353b56dada235ee0973ab4b46cbe5ea5cbe
maneater2 <pielover@tuta.io>
2025-07-10 20:21:17 +0300

    h

--- services/_cloudflared.nix
+++ services/_cloudflared.nix
@@ -7,7 +7,7 @@
   sops.secrets = {
     "cloudflare-tunnel" = {
       format = "json";
-      sopsFile = ./../secrets/cloudflare-tunnel.json;
+      sopsFile = ./../secrets/cloudflare-tunnel;
     };
     "cloudflare-token" = {
       format = "binary";
