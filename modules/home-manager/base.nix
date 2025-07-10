d5888094ebede2fb79a3c6d11b519561eb4db86d
maneater2 <pielover@tuta.io>
2025-07-10 12:35:43 +0300

    fixes

--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -12,33 +12,13 @@
     ./_zsh.nix
   ];
 
-  nixpkgs = {
-    # You can add overlays here
-    overlays = [
-      # If you want to use overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
-    ];
-    # Configure your nixpkgs instance
-    config = {
-      # Disable if you don't want unfree packages
-      allowUnfree = true;
-      # Workaround for https://github.com/nix-community/home-manager/issues/2942
-      allowUnfreePredicate = _: true;
-    };
-  };
-
-  # TODO: Set your username
   home = {
     username = vars.userName;
     homeDirectory = "/home/${vars.userName}";
     stateVersion = "25.05";
+    sessionVariables = {
+      SOPS_AGE_KEY_FILE "$HOME/.config/sops/age/keys.txt";
+    };
   };
 
   programs = {
