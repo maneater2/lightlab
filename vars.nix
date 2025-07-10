757dca88bd40981f8c3df2ae4504a9539e8f8ff9
maneater2 <pielover@tuta.io>
2025-07-10 12:41:33 +0300

    fix

--- vars.nix
+++ vars.nix
@@ -2,5 +2,5 @@
   fullName = "Light";
   userName = "light";
   userEmail = "pielover@tuta.io";
-  sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPbYUvHahjCoxS9gm3oqZuUEk1Ovl7EjeHtMSLUhNeJ"
+  sshPublicKeyPersonal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHPbYUvHahjCoxS9gm3oqZuUEk1Ovl7EjeHtMSLUhNeJ";
 }
