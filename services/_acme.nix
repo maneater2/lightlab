c1322a8b5204b4551137a8bba9a1d169eee010b6
maneater2 <pielover@tuta.io>
2025-07-10 19:14:44 +0300

    acme

---
+++ services/_acme.nix
@@ -0,0 +1,36 @@
+{config, ...}: {
+  sops.secrets = {
+    "cloudflare-api-email" = {};
+    "cloudflare-api-key" = {};
+  };
+
+  # inspo: https://carjorvaz.com/posts/setting-up-wildcard-lets-encrypt-certificates-on-nixos/
+  security.acme = {
+    acceptTerms = true;
+    defaults.email = "admin+acme@balticumvirtus.com";
+
+    certs."balticumvirtus.com" = {
+      domain = "balticumvirtus.com";
+      extraDomainNames = ["*.balticumvirtus.com"];
+      dnsProvider = "cloudflare";
+      dnsPropagationCheck = true;
+      # inspo: https://go-acme.github.io/lego/dns/cloudflare/
+      credentialFiles = {
+        "CLOUDFLARE_DNS_API_TOKEN_FILE" = config.sops.secrets."cloudflare-api-key".path;
+      };
+    };
+  };
+
+  users.users.nginx.extraGroups = ["acme"];
+
+  networking.firewall.allowedTCPPorts = [
+    80
+    443
+  ];
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      "/var/lib/acme"
+    ];
+  };
+}
