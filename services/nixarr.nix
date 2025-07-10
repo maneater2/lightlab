18c961849942c63394d59c6370639b7646d46b76
maneater2 <pielover@tuta.io>
2025-07-10 19:58:14 +0300

    wg

---
+++ services/nixarr.nix
@@ -0,0 +1,126 @@
+{
+  config,
+  pkgs,
+  vars,
+  ...
+}: {
+  imports = [
+    ./_acme.nix
+    ./_nginx.nix
+    ./_cloudflared.nix
+  ];
+
+  nixarr = {
+    enable = true;
+    mediaDir = "/fun";
+    stateDir = "/var/lib/nixarr";
+
+    jellyfin.enable = true;
+    prowlarr.enable = true;
+    radarr.enable = true;
+    sonarr.enable = true;
+
+    transmission = {
+      enable = true;
+      package = pkgs.transmission_4;
+      # todo: figure out how to update this easier
+      peerPort = 46634;
+      vpn.enable = true;
+      extraSettings = {
+        incomplete-dir-enabled = false;
+        speed-limit-up = 500;
+        speed-limit-up-enabled = true;
+        rpc-authentication-required = true;
+        rpc-username = vars.userName;
+        rpc-whitelist-enabled = false;
+        # todo: figure out how to integrate rpc-password into sops-nix
+        rpc-password = "{651f719907abc16376ca8010851893abcf6ef1d1rVakBQJO";
+      };
+    };
+
+    vpn = {
+      enable = true;
+      wgConf = config.sops.secrets."wg.conf".path;
+    };
+  };
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override {enableHybridCodec = true;};
+  };
+
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
+      intel-media-driver
+      libvdpau-va-gl
+      vaapiIntel
+      vaapiVdpau
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    # To enable `intel_gpu_top`
+    intel-gpu-tools
+    # because nixarr does not include it by default
+    wireguard-tools
+  ];
+
+  services.nginx = {
+    virtualHosts = {
+      "watch.balticumvirtus.com" = {
+        forceSSL = true;
+        useACMEHost = "balticumvirtus.com";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://127.0.0.1:8096";
+        };
+      };
+
+      "prowlarr.balticumvirtus.com" = {
+        forceSSL = true;
+        useACMEHost = "balticumvirtus.com";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://127.0.0.1:9696";
+        };
+      };
+
+      "radarr.balticumvirtus.com" = {
+        forceSSL = true;
+        useACMEHost = "balticumvirtus.com";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://127.0.0.1:7878";
+        };
+      };
+
+      "sonarr.balticumvirtus.com" = {
+        forceSSL = true;
+        useACMEHost = "balticumvirtus.com";
+        locations."/" = {
+          recommendedProxySettings = true;
+          proxyPass = "http://127.0.0.1:8989";
+        };
+      };
+
+      "transmission.balticumvirtus.com" = {
+        forceSSL = true;
+        useACMEHost = "balticumvirtus.com";
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:9091";
+        };
+      };
+    };
+  };
+
+  systemd = {
+    tmpfiles.rules = ["d /var/lib/nixarr 0755 root root"];
+  };
+
+  environment.persistence."/nix/persist" = {
+    directories = [
+      "/var/lib/nixarr"
+    ];
+  };
+}
