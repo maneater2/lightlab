d1c65b862b93591532104a6cb082b336ac1c7244
maneater2 <pielover@tuta.io>
2025-07-12 16:39:47 +0300

    fixes

---
+++ machines/strawberrypie/configuration.nix
@@ -0,0 +1,32 @@
+{
+  inputs,
+  outputs,
+  vars,
+  ...
+}: {
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+    inputs.nixos-wsl.nixosModules.default
+    inputs.vscode-server.nixosModules.default
+
+    ./hardware-configuration.nix
+
+    ./../../modules/wsl/base.nix
+  ];
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs outputs vars;};
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    users = {
+      ${vars.userName} = {
+        imports = [
+	  ./../../modules/home-manager/git.nix
+          ./../../modules/home-manager/base.nix
+        ];
+      };
+    };
+  };
+
+  networking.hostName = "carrotcake";
+}
