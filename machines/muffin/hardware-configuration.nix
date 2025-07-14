8566850b3b98577362ef181b26668677bfbd18c9
maneater2 <pielover@tuta.io>
2025-07-14 12:57:30 +0300

    stuff

--- machines/muffin/hardware-configuration.nix
+++ machines/muffin/hardware-configuration.nix
@@ -1,44 +1,47 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  config,
+  lib, 
+  modulesPath, 
+  ... 
+}: {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot = {
+    kernelModules = [ "kvm-intel" ];
+    initrd = {
+    availableKernelModules = [ "xhci_pci" "nvme" "usb_storage" "sd_mod" "iwlwifi" ];
+      luks = {
+        reusePassphrases = true;
+        devices = {
+          "cryptroot" = {
+            device = "/dev/disk/by-uuid/7c9a3d73-7927-45ae-a29b-f16a6c791bd8";
+	    allowDiscards = true;
+	  };
+	};
+      };
+    };
+  };
 
-  fileSystems."/" =
-    { device = "none";
+  filesystems = {
+    "/" = {
+      device = "none";
       fsType = "tmpfs";
       options = ["defaults" "size=4G" "mode=0755"];
     };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/3570-B316";
+    "/boot" = {
+      device = "/dev/disk/by-label/boot";
       fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
+      options = ["umask=0077"];
     };
-
-  fileSystems."/nix" =
-    { device = "/dev/disk/by-uuid/962e77e3-3ae6-4c51-b32d-5ba94cb2f949";
+    "nix" = {
+      device = "/dev/disk/by-label/Nix";
       fsType = "ext4";
     };
+  };
 
-  boot.initrd.luks.devices."cryptroot".device = "/dev/disk/by-uuid/7c9a3d73-7927-45ae-a29b-f16a6c791bd8";
-
-  swapDevices = [ ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp4s0.useDHCP = lib.mkDefault true;
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
