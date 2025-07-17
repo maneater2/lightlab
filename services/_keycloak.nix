08afb9d769caa8b44e354a64077b1f81ce0ba328
maneater2 <pielover@tuta.io>
2025-07-17 19:49:16 +0300

    screw this im using the wiki

--- services/_keycloak.nix
+++ services/_keycloak.nix
@@ -3,10 +3,7 @@
   pkgs,
   lib,
   ...
-}: let
-  fqdn = "keycloak.balticumvirtus.com";
-in
-{
+}: {
 
   imports = [
     ./_acme.nix
@@ -17,32 +14,49 @@ in
 
   services.keycloak = {
     enable = true;
-    initialAdminPassword = config.sops.secrets."keycloak-pass".path;
-    database.type = "h2"; # im too lazy for postgresql
+#    themes = with pkgs; [
+#      custom = custom_keycloak_themes.custom;
+#    ];
+    database = {
+      type = "postgresql";
+      createLocally = true;
+
+      username = "keycloak";
+      passwordFile = config.sops.secrets."keycloak-pass".path;
+    };
     settings = {
       spi-theme-static-max-age = "-1";
       spi-theme-cache-themes = false;
       spi-theme-cache-templates = false;
       http-port = 8821;
-      hostname = fqdn;
+      hostname = "cloak.balticumvirtus.com";
       hostname-strict = false;
       hostname-strict-https = false;
       proxy-headers = "xforwarded";
       http-enabled = true;
+      proxy = "passthrough";
     };
   };
 
-  services.nginx.virtualHosts."${fqdn}" = {
+  services.nginx.virtualHosts."keycloak.balticumvirtus.com" = {
     forceSSL = true;
     useACMEHost = "balticumvirtus.com";
 
     locations."/" = {
-      proxyPass = "http://127.0.0.1:8821";
-      proxyWebsockets = true;
+      proxyPass = "http://localhost:${toString config.services.keycloak.settings.http-port}";
     };
   };
 
   environment.persistence."/nix/persist" = {
     directories = [ "/var/lib/keycloak" ]; # also add a directory for postgresql if nextcloud.nix is not used, can't have it on both files at the same time
   };
+
+  environment.systemPackages = with pkgs; [
+    keycloak
+    #custom_keycloak_themes.verydeliciouspie # TODO
+  ];
+
+  services.postgresql.enable = true;
+
+  environment.noXlibs = false; # this was in the manual ok
 }
